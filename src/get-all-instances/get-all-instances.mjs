a5811f6f1938f3e61a95c0d3865087f738bf78d2
zahir535 <zahirco535@gmail.com>
2025-08-20 13:15:56 +0800

    refactor: get all instances lambda to return simplified response

--- src/get-all-instances/get-all-instances.mjs
+++ src/get-all-instances/get-all-instances.mjs
@@ -1,9 +1,8 @@
 // File: src/get-all-instances/get-all-instances.js
 import { EC2Client, DescribeInstancesCommand } from "@aws-sdk/client-ec2";
 
-export const getAllInstancesHandler = async () => {
-  const ec2 = new EC2Client({ region: 'ap-southeast-1' });
-
+export const getAllInstancesHandler = async (event) => {
+  const ec2 = new EC2Client({ region: "ap-southeast-1" });
   try {
     // 1. Get all running instances
     const describeCmd = new DescribeInstancesCommand({});
@@ -12,16 +11,25 @@ export const getAllInstancesHandler = async () => {
 
     // Extract instance IDs
     const instanceLists = [];
-    describeResult.Reservations?.forEach(reservation => {
-      reservation.Instances?.forEach(instance => {
-        instanceLists.push(instance);
+    describeResult.Reservations?.forEach((reservation) => {
+      reservation.Instances?.forEach((instance) => {
+        if (event?.queryStringParameters?.inDetails) {
+          instanceLists.push(instance);
+        } else {
+          instanceLists.push({
+            instanceId: instance.InstanceId,
+            Tags: instance.Tags,
+            State: instance.State,
+            PublicDnsName: instance.PublicDnsName,
+          });
+        }
       });
     });
 
     if (instanceLists.length === 0) {
       return {
         statusCode: 200,
-        body: JSON.stringify({ message: "No instances found." })
+        body: JSON.stringify({ message: "No instances found." }),
       };
     }
 
@@ -30,14 +38,13 @@ export const getAllInstancesHandler = async () => {
       body: JSON.stringify({
         message: "All instances",
         instances: instanceLists,
-      })
+      }),
     };
-
   } catch (error) {
     console.error("Error stopping instances:", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message })
+      body: JSON.stringify({ error: error.message }),
     };
   }
 };
